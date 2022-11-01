d6370ba692df5ea047293250f6d015da219619ff
Yauheni Rudzko <e-rudko2000@mail.ru>
2022-11-01 21:41:57 +0300

    Count of pizza

--- src/main/java/com/epam/rd/autotasks/pizzasplit/PizzaSplit.java
+++ src/main/java/com/epam/rd/autotasks/pizzasplit/PizzaSplit.java
@@ -1,11 +1,21 @@
 package com.epam.rd.autotasks.pizzasplit;
+import java.util.Scanner;
 
 public class PizzaSplit {
     public static void main(String[] args) {
-        //Write a program, reading number of people and number of pieces per pizza and then
-        //printing minimum number of pizzas to order to split all the pizzas equally and with no remainder
-
-
-
+        Scanner scanner = new Scanner(System.in);
+        int numberOfPeople = scanner.nextInt();
+        int numberOfPieces = scanner.nextInt();
+        int numberOfPizza = 1;
+        int fixPieces = numberOfPieces;
+        while (true) {
+            numberOfPieces = fixPieces;
+            numberOfPieces *= numberOfPizza;
+            if(numberOfPieces % numberOfPeople == 0){
+                break;
+            }
+            numberOfPizza++;
+        }
+        System.out.println(numberOfPizza);
     }
 }
